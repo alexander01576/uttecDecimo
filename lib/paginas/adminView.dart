ed73d7424ec83563c909c324a144f79f6dc1943f
Alexander <alexandermendez01576@gmail.com>
2023-02-15 01:51:51 -0600

    Se agrego interfaz de admin y validacion en caso de que sea el admin

---
+++ lib/paginas/adminView.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AdminView extends StatefulWidget {
+  const AdminView({Key? key}) : super(key: key);
+
+  @override
+  State<AdminView> createState() => _AdminViewState();
+}
+
+class _AdminViewState extends State<AdminView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Administrador"),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
